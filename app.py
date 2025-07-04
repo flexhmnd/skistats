eee00600bc01c6d76e0c0e38b01017dcc4181002
flexhmnd <flexhmnd@gmail.com>
2025-07-04 15:18:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ dash_app.layout = html.Div(style={
                 'left': '0',
                 'width': '100%',
                 'height': '100%',
-                'backgroundColor': 'rgba(255, 255, 255, 0.6)',
+                'backgroundColor': 'rgba(255, 255, 255, 0.9)',
                 'display': 'flex',
                 'justifyContent': 'center',
                 'alignItems': 'center',
